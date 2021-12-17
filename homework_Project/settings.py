67360f46876e22c09b2e8dfea19a1a2244bb5618
vanyagorev14 <vaneeokk@gmail.com>
2021-12-17 22:06:22 +0200

    push

--- homework_Project/settings.py
+++ homework_Project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-23br%@8m@hhkcx&g5(!ytmoizcj(gow(ad!sh7yha4cbxaq=x3'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-23br%@8m@hhkcx&g5(!ytmoizcj(gow(ad!sh7yha4cbxaq=x3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'polls',
+
 ]
 
 MIDDLEWARE = [
